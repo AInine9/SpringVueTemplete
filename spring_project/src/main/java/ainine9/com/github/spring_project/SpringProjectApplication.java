627b2c528e2924e310c3678e7e2551e9d4d6e61a
AInine <ainine.jp@gmail.com>
2022-05-24 23:41:08 +0900

    initial commit

---
+++ spring_project/src/main/java/ainine9/com/github/spring_project/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package ainine9.com.github.spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
